19182119432369a5b875a4002f3d905d8915b505
origin090 <usb0101@naver.com>
2026-04-27 13:03:13 +0900

    [Bronze I] Title: 평균, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.py
@@ -0,0 +1,12 @@
+N = int(input())
+scores = list(map(int, input().split()))
+
+M = max(scores)
+
+new_scores = []
+for score in scores:
+    new_scores.append(score / M * 100)
+
+average = sum(new_scores) / N
+
+print(average)
